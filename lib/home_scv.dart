8206f391ef3927e5fb9e504008ec974dd5ae36b4
GajiHabib <gmhabiburrahman295@gmail.com>
2023-11-26 21:33:05 +0600

    farstcommit

--- lib/home_scv.dart
+++ lib/home_scv.dart
@@ -1,10 +1,46 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget {
-  const home({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('image view')),
+      ),
+      body: Center(
+          child: Container(
+            height:600,
+            width: 600,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(16))),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                  child: Image.network(
+                    'https://deep-image.ai/blog/content/images/2022/09/underwater-magic-world-8tyxt9yz.jpeg',
+                    height: 275,
+                    width: 275,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset(
+                    'assets/ha.jpeg',
+                    width: 200,
+                  ),
+                )
+              ],
+            ),
+          ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:projectquiz/home_scv.dart';
 
 
 void main() {
@@ -20,30 +21,34 @@ class _NewAppState extends State<NewApp> {
   // Color myColor = Colors.blue;
 
   // int myNumber = 10;
-  List<String> hok=['dhaka','khulna','brishal','khirati',];
+  // List<String> hok=['dhaka','khulna','brishal','khirati',];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('pructis')),
-        ),
-        body: ListView.builder(
-          itemCount: hok.length,
-          itemBuilder: (context, index) {
-          return Card(
-            child: Container(
-                  height: 70,
-                  margin: EdgeInsets.all(10),
-                  color: Colors.amber,
-                  child: Center(child: Text(hok[index]),),
-          
-            ),
-          );
-        },)
-      ),
+      home: HomePage(),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //       title: Center(
+      //           child: Text(
+      //     'practise',
+      //     style: TextStyle(fontFamily: AutofillHints.addressCity, fontSize: 50),
+      //   ))),
+      // ),
+      // body: ListView.builder(
+      //   itemCount: hok.length,
+      //   itemBuilder: (context, index) {
+      //   return Card(
+      //     color: Color.fromARGB(255, 244, 248, 248),
+      //     child: Container(
+      //           height: 70,
+      //           margin: EdgeInsets.all(10),
+      //           color: Colors.amber,
+      //           child: Center(child: Text(hok[index]),),
+      //     ),
+      //   );
+      // },)
     );
   }
 }
